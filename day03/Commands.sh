bfbd53f542c59d462d426592990163a98039ca57
coffeethis <105961896+coffeethis@users.noreply.github.com>
2022-06-08 22:49:40 +0800

    Update Commands.sh

--- day03/Commands.sh
+++ day03/Commands.sh
@@ -44,3 +44,24 @@ watch kubectl get all -nworkshop03
 
 # Install Ingress Controller so that IngressClass can work
 helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace
+
+---
+#workshop03 intro
+
+#helm repo list
+#helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace
+
+#watch kctl get all -n ingress-nginx
+
+#helm ls -A
+
+#kctl get /secret/sealed-secrets-keyxxxx -n sealed-secrets -oyaml
+
+# cat cert.crt | base64 -d 
+#openssl x509 -in ss.cert -noout -text | less
+
+# within the layer folder
+#kctl apply -k . oyaml --dry-run=client
+
+#traffic gen
+#siege -c20 -v URL
